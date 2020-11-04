7e45f77f6bf543ec47322244e629f83757ecfc42
nickslick09 <nikhilronaldo97@gmail.com>
2020-11-04 12:21:38 +0530

    sample

--- sample.py
+++ sample.py
@@ -1,6 +1,6 @@
 # import the necessary packages
 from cv2 import cv2
 # load the image and show it
-image = cv2.imread("‪lfc.jpg")
+image = cv2.imread("‪Captcha (1).png")
 cv2.imshow("original", image)
 cv2.waitKey(0)
