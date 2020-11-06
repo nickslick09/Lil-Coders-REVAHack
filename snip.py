a839bdd79b23138b2ca58c464393f5453b10318c
kamalesh1810 <61874657+kamalesh1810@users.noreply.github.com>
2020-11-06 23:41:42 +0530

    Update snip.py

--- snip.py
+++ snip.py
@@ -20,19 +20,17 @@ pw=x[1]
 emailid=x[2]
 file_object.close()
 driver.get('https://erp.reva.edu.in/REVAUniversity/StudentLogin.do')
+
+captchaName = 'captcha.png'
+image=driver.find_element_by_xpath('//*[@id="captcha"]').screenshot(captchaName) # saves screenshot
+decodedCaptcha = Decode.getCaptcha(captchaName)
 username=driver.find_element_by_name("userName")
 username.clear()
 username.send_keys(uname)
 password=driver.find_element_by_name("password")
 password.clear()
 password.send_keys(pw)
-
-captchaName = 'captcha.png'
-image=driver.find_element_by_xpath('//*[@id="captcha"]').screenshot(captchaName) # saves screenshot
-
-decodedCaptcha = Decode.getCaptcha(captchaName)
 print(decodedCaptcha)
-
 cap=driver.find_element_by_name("captchaValue")
 cap.clear()
 cap.send_keys(decodedCaptcha)
