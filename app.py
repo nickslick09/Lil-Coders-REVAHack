c40a85800fc5b231dc323825528a579d0559a4a8
kamalesh1810 <61874657+kamalesh1810@users.noreply.github.com>
2020-11-06 21:44:09 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,52 @@
+from flask import Flask
+from flask import render_template
+from flask import request,redirect,url_for,Response
+import os 
+app=Flask(__name__)
+@app.route('/',methods=["POST", "GET"])
+def home():
+    if request.method=="POST":
+        user=request.form["userid"]
+        password=request.form["pswrd"]
+        emailid=request.form["email"]
+        return redirect(url_for("users",usr=user,pw=password,ei=emailid))
+    else:
+        return render_template("index.html")
+
+def shutdown_server():
+    func = request.environ.get('werkzeug.server.shutdown')
+    if func is None:
+        raise RuntimeError('Not running with the Werkzeug Server')
+    func()
+
+#@app.route('/shutdown', methods=['GET','POST'])
+#def shutdown():
+#    shutdown_server()
+#    return 'Thank you for entering your response'
+
+@app.route('/<usr>/<pw>/<ei>', methods=['GET','POST'])
+def users(usr,pw,ei):
+    file_object = open('currentuserdetails.txt', 'a')
+    file_object2 = open('userdetailscomplete.txt', 'a')
+    file_object. truncate(0)
+    file_object.write(usr)
+    file_object.write('\n')
+    file_object.write(pw)
+    file_object.write('\n')
+    file_object.write(ei)
+    file_object.write('\n')
+    file_object2.write(usr)
+    file_object2.write('\n')
+    file_object2.write(pw)
+    file_object2.write('\n')
+    file_object2.write(ei)
+    file_object2.write('\n')
+    file_object.close()
+    file_object2.close()
+    shutdown_server()
+    return render_template("endpage.html")
+
+if __name__=="__main__":
+    app.run(debug=False)
+
+os.system('snip.py')
--- snip.py
+++ snip.py
@@ -1,15 +1,25 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-#import time
+import time
 PATH=("C:\Program Files\chromedriver.exe")
 driver = webdriver.Chrome(PATH)
+file_object = open('currentuserdetails.txt', 'r')
+Lines = file_object.readlines() 
+x=[]
+for line in Lines:
+    y=line.split()
+    x.append(y)
+uname=x[0]
+pw=x[1]
+emailid=x[2]
+file_object.close()
 driver.get('https://erp.reva.edu.in/REVAUniversity/StudentLogin.do')
 username=driver.find_element_by_name("userName")
 username.clear()
-username.send_keys("R18CS343")
+username.send_keys(uname)
 password=driver.find_element_by_name("password")
 password.clear()
-password.send_keys("xxxxxx")
+password.send_keys(pw)
 image=driver.find_element_by_xpath('//*[@id="captcha"]').screenshot('captcha.png') # saves screenshot
 driver.find_element_by_id("Login").click()
-#driver.quit() for closing the browser
+#driver.quit() #for closing the browser
