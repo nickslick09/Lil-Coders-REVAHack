bbd63fb65ec5f78e0cad33206d3e4ae1083c2659
kamalesh1810 <61874657+kamalesh1810@users.noreply.github.com>
2020-11-07 15:12:42 +0530

    Delete tesseract.py

--- tesseract.py
+++
@@ -1,4 +0,0 @@
-from PIL import Image
-import pytesseract
-pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
-print(pytesseract.image_to_string(Image.open("image.png"),lang="eng",config=' -c tessedit_char_whitelist=abcdefghijklmnopqrstuvwxyz0123456789'))
